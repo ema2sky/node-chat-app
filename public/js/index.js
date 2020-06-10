ad1f774e887a8a779e7565a1004a007d7bc1b226
emmanuel avwenake <imma4success@gmail.com>
2020-06-10 15:40:56 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+	//Selectors
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child')
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+};
+
         socket.on('connect',function () {
             console.log('Connected to server');
        })
@@ -16,7 +32,8 @@ var socket = io();
                 createdAt: formattedMessage
             });
 
-            $('#messages').append(html);
+						$('#messages').append(html);
+						scrollToBottom();
         });
 
         socket.on('newLocationMessage', function (message){
@@ -27,7 +44,8 @@ var socket = io();
 							createdAt:formattedMessage,
 							url:message.url
 						})
-            $('#messages').append(html);
+						$('#messages').append(html);
+						scrollToBottom();
         })
 
         $('#message-form').on('submit', function(e){
